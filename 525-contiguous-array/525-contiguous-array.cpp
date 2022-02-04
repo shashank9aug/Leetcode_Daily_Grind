0c66b9226267578684efbe811e7759254ba564d2
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-02-04 10:27:11 +0530

    Time: 272 ms (12.48%), Space: 83.8 MB (55.07%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,12 +3,12 @@ public:
     int findMaxLength(vector<int>& nums) {
         int n = nums.size();
         
-        //make all 0-> -1 for making sum==0
-        for(int &it:nums){
-            if(it==0){
-                it=-1;
-            }
-        }
+        // //make all 0-> -1 for making sum==0
+        // for(int &it:nums){
+        //     if(it==0){
+        //         it=-1;
+        //     }
+        // }
         // for(int i=0;i<n;++i){
         //     if(nums[i]==0){
         //         nums[i]=-1;
@@ -16,24 +16,31 @@ public:
         // }
         
         //create prefix array sum
-        vector<int>pre;
-        pre.push_back(0);
-        for(auto it:nums){
-            pre.push_back(pre.back()+it);
-        }
+        // vector<int>pre;
+        // pre.push_back(0);
+        // for(auto it:nums){
+        //     pre.push_back(pre.back()+it);
+        // }
         
         //for making pre(r) - pre(l-1) = sum of subarray = 0
         //so, pre(r) = pre(l-1) 
         unordered_map<int,int>ump;
+        ump[0] = 0;
         int ans = 0;
-        for(int i=0;i<size(pre);i++){
-            int num = pre[i];
-            auto it = ump.find(num);
-            if(it!=ump.end()){
-                ans = max(ans,i-ump[num]);
+        int pref = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                pref += -1;
             }
             else{
-                ump[num] = i;
+                pref += 1;
+            }
+            auto it = ump.find(pref);
+            if(it!=ump.end()){
+                ans = max(ans,i + 1 - ump[pref]);
+            }
+            else{
+                ump[pref] = i + 1;
             }
         }
         return ans;
