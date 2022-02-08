2859533cfd34a813704e9e9cd9bce97b9e6c95bc
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-02-08 11:48:44 +0530

    Time: 4 ms (43.34%), Space: 5.8 MB (75.31%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10){
+            return num;
+        }
+        
+        int sum = 0;
+        while(num>=10){
+            sum = 0;
+            while(num!=0){
+                sum += num%10;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return sum;
+    }
+};
