86e8914be47d60050ddd4d5f8bc2011687f18639
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-03-06 01:32:02 +0530

    Time: 15 ms (38.97%), Space: 11.9 MB (28.15%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:    
+int dp[10001];
+    int houseRobber(vector<int>& arr, int idx){
+        if(idx>=arr.size())
+            return 0;
+        if(dp[idx] != -1)
+            return dp[idx];
+        int op1 = arr[idx]+houseRobber(arr,idx+2);
+        int op2 = houseRobber(arr,idx+1);
+        return dp[idx] = max(op1,op2);
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        memset(dp,-1,sizeof dp);
+        vector<int> a(10001,0);
+        for(auto num : nums)
+            a[num] += num;
+        return houseRobber(a,0);
+    }
+};
