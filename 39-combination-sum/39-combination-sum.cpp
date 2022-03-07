6ce989ff7d5d39f08478f217b7753baf7f30cb60
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-03-07 17:16:33 +0530

    Time: 7 ms (80.64%), Space: 10.8 MB (71.93%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,30 @@
 class Solution {
-public: 
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
-        if(ind == arr.size()) {
-            if(target == 0) {
-                ans.push_back(ds); 
+private:
+    void findCombination(vector<int>&arr,vector<int>&ds,vector<vector<int>>&ans,int target,int idx){
+        //Base case:
+        if(idx==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
             }
-            return; 
-        }
-        // pick up the element 
-        if(arr[ind] <= target) {
-            ds.push_back(arr[ind]); 
-            findCombination(ind, target - arr[ind], arr, ans, ds); 
-            ds.pop_back(); 
+            return;
         }
         
-        findCombination(ind+1, target, arr, ans, ds); 
-        
+        //rec call
+        //pick up the element : 
+        if(arr[idx]<=target){
+            ds.push_back(arr[idx]);
+            //we are picking up the same element many times according to question
+            findCombination(arr,ds,ans,target-arr[idx],idx);
+            ds.pop_back();
+        }
+        //not pickup the element
+        findCombination(arr,ds,ans,target,idx+1);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans; 
-        vector<int> ds; 
-        findCombination(0, target, candidates, ans, ds); 
-        return ans; 
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(candidates,ds,ans,target,0);
+        return ans;
     }
 };
