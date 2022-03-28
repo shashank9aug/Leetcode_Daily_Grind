ac2386cacbfb5fcbd4c84425c117e523fcc58386
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-03-28 14:04:39 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (29.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        int idx = findMinIdx(nums);
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            int mid = (m + idx) % nums.size();
-            if (nums[mid] == target)
-            {
+        int start = 0;
+        int end = size(nums) - 1;
+    
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] == target) 
                 return mid;
-            }
-            if (nums[mid] < target) 
-            {
-                l = m + 1;
+
+            // the following is the same as problem I
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } 
+                else
+                {
+                    start = mid + 1;
+                }
             } 
             else
             {
-                r = m - 1;
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
-    }
-private:
-    int findMinIdx(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-        while (l < r && nums[l] > nums[r]) 
-        {
-            int m = l + (r - l) / 2;
-            if (nums[m] > nums[r]) 
-            {
-                l = m + 1;
-            } 
-            else
-            {
-                r = m;
-            }
-        }
-        return l;
-    }
+    }    
 };
-
