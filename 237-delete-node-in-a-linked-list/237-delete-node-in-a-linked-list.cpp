4b3540f255dcd50425dff09a59837909bbe9681a
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-05-06 13:44:51 +0530

    Time: 17 ms (44.14%), Space: 7.5 MB (92.68%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -9,8 +9,15 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        swap(node->val,node->next->val);
-        ListNode*temp = node->next;
-        node->next = node->next->next;
+       //their are two senario node can be last node of linked list or middle node of ll
+        if(node->next==NULL){
+            delete node;
+        }
+        else{
+            swap(node->val,node->next->val);
+            ListNode*temp = node->next;
+            node->next = node->next->next;
+            delete temp;
+        }
     }
 };
