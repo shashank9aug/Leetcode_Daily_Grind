17567afc4c5a4bae3eb8681dd2f19651d3d78ea3
Shashank Shekhar <shekhar1245shashank@gmail.com>
2022-05-27 02:09:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.10%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int ans = 0;
-        while(n > 0)
-        {
-            // if last bit of n is set or not just check it and eliminate it
-            if(n%2==1)
-                ans++;
-            n /= 2;
+        int cnt = 0;
+        
+        while(n){
+            n = n & (n-1);
+            cnt++;
         }
-        return ans;
+        return cnt;
     }
 };
